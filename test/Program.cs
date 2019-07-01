8c50ce3db1c6d4538b6f581b1658ba371f77c31b
Jesse van Leeuwen <sf.jessenl@gmail.com>
2019-07-01 17:35:42 +0200

    FEAT: better code benchmarking

--- test/Program.cs
+++ test/Program.cs
@@ -17,7 +17,6 @@ namespace test
 
             // performance check sdn parsing
             TimedCall( "parse sdn file", ()=> parser = new Parser(text), testCount);
-            
             // performance check getters
             TimedCall( "invenory [0]", ()=> parser.GetToken("scene.objects.player.inventory[0]"), testCount );
             TimedCall( "data [0]", ()=> parser.GetToken("data[0]"), testCount );
@@ -25,12 +24,21 @@ namespace test
 
         private static void TimedCall(string p_name, TimedCall p_call, int p_amount = 1)
         {
+            // source: http://www.bradoncode.com/blog/2012/02/benchmarking-methods-in-c-made-easier.html
+
+            // clean up
+            System.GC.Collect();
+            System.GC.WaitForPendingFinalizers();
+            System.GC.Collect();
+            //warm up
+            p_call();
+
             Stopwatch stopwatch = Stopwatch.StartNew();
             for(int i = 0; i < p_amount; ++i)
                 p_call.Invoke();
-            
+
             stopwatch.Stop();
-            Console.WriteLine("operation {1} executed: {2} took: {0}", stopwatch.Elapsed, p_name, p_amount);
+            Console.WriteLine("operation {1} executed: {2} times in: {0}", stopwatch.Elapsed, p_name, p_amount);
             Console.WriteLine("avarage execution time: {0} \n", stopwatch.Elapsed / p_amount);
         }
     }
